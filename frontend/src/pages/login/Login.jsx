683ce07a127fcf58d3eed9018cff4cc496f9fa64
luigi059 <luigimarkfernandez@gmail.com>
2024-05-02 22:55:13 +0100

    work started on FE

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+const Login = () => {
+	return (
+		<div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+			<div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+				<h1 className="text-3x1 font-semibold text-center text-gray-300">
+					Login
+				</h1>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
