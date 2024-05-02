5aee8508672f17ab8bb3ab67186f06fc2754902b
luigi059 <luigimarkfernandez@gmail.com>
2024-05-02 15:06:25 +0100

    get messages api endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -34,3 +34,22 @@ export const sendMessage = async (req, res) => {
 		res.status(400).json({ error: 'Internal Server Error' });
 	}
 };
+
+export const getMessages = async (req, res) => {
+	try {
+		const { id: userToChatId } = req.params;
+		const senderId = req.user;
+
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate('messages'); // NOT REFERENCES BUT ACTUAL MESSAGES
+
+		if (!conversation) return res.status(200).json([]);
+		const messages = conversation.messages;
+
+		res.status(200).json(messages);
+	} catch (error) {
+		console.log('Error in getMessages controller: ', error.message);
+		res.status(400).json({ error: 'Internal Server Error' });
+	}
+};
