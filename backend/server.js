6b5f95413f478fa758b135c37e0b307dd437ee3e
luigi059 <luigimarkfernandez@gmail.com>
2024-05-02 15:27:38 +0100

    get users BE

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(cookieParser()); // to parse the incoming cookies with req.cookies
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 /* app.get('/', (req, res) => {
 	// route route http://localhost:5000/
