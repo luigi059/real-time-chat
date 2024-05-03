86c0d77e332b333fe135033f1294022627dc0592
luigi059 <luigimarkfernandez@gmail.com>
2024-05-03 19:01:14 +0100

    Context API WIP

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -36,7 +36,11 @@ const useSignup = () => {
 			});
 
 			const data = await res.json();
-			console.log(data);
+			if (data.error) {
+				throw new Error(data.error);
+			}
+			// localstorge
+			// context
 		} catch (error) {
 			toast.error(error.message);
 		} finally {
