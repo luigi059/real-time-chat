90eb3b8cc1bdf151eb56da9bfd817602c7a40f7b
luigi059 <luigimarkfernandez@gmail.com>
2024-05-04 13:02:25 +0100

    Context

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
 
 const useSignup = () => {
 	const [loading, setLoading] = useState(false);
+	const { setAuthUser } = useAuthContext();
 
 	const signup = async ({
 		fullName,
@@ -39,8 +41,9 @@ const useSignup = () => {
 			if (data.error) {
 				throw new Error(data.error);
 			}
-			// localstorge
-			// context
+
+			localStorage.setItem('chat-user', JSON.stringify(data));
+			setAuthUser(data);
 		} catch (error) {
 			toast.error(error.message);
 		} finally {
