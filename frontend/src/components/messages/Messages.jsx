a366f1e39b3e37d0c7b59575ff55733f9fb63f40
luigi059 <luigimarkfernandez@gmail.com>
2024-05-04 16:05:46 +0100

    Get Messages Functionality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useRef } from 'react';
+import useGetMessages from '../../hooks/useGetMessages';
+import MessageSkeleton from '../skeletons/MessageSkeleton';
 import Message from './Message';
 
 const Messages = () => {
+	const { loading, messages } = useGetMessages();
+	const lastMessageRef = useRef();
+
+	useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+		}, 100);
+	}, [messages]);
+
 	return (
 		<div className="px-4 flex-1 overflow-auto">
-			<Message />
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message key={message._id} message={message} />
+					</div>
+				))}
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className="text-center">Send a message to start a conversation</p>
+			)}
 		</div>
 	);
 };
