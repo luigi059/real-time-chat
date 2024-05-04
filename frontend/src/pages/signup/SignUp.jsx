055bccf75cb79fbd127ac25b8a969226ebd76a9d
luigi059 <luigimarkfernandez@gmail.com>
2024-05-04 13:39:12 +0100

    Login Functionality

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -108,7 +108,16 @@ const SignUp = () => {
 						Already have an account?
 					</Link>
 					<div>
-						<button className="btn btn-block btn-sm mt-2">Sign Up</button>
+						<button
+							className="btn btn-block btn-sm mt-2 border border-slate-700"
+							disabled={loading}
+						>
+							{loading ? (
+								<span className="loading loading-spinner"></span>
+							) : (
+								'Sign Up'
+							)}
+						</button>
 					</div>
 				</form>
 			</div>
