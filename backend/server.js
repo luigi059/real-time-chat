8bffd3de09275ad91117af9ca869de936535e683
luigi059 <luigimarkfernandez@gmail.com>
2024-05-05 16:56:42 +0100

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -8,6 +9,7 @@ import userRoutes from './routes/user.routes.js';
 import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,10 +20,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-/* app.get('/', (req, res) => {
-	// route route http://localhost:5000/
-	res.send('Server Ready');
-}); */
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
